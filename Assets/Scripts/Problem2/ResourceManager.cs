19864047e1f1b98c3cf3950e9118e689f73215bc
luckynee <janspurba64@gmail.com>
2021-12-16 02:46:36 +0700

    problem6

---
+++ Assets/Scripts/Problem2/ResourceManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public GameObject resourcePrefabs;
+    public int count = 5;
+
+    [Header("Respawn Radius")]
+    public Transform xPoint;
+    public Transform yPoint;
+    public float offset = 0f;
+
+    private float _xRadius;
+    private float _yRadius;
+
+    private void Awake()
+    {
+        _xRadius = xPoint.position.x;
+        _yRadius = yPoint.position.y;
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < count; i++)
+            SpawnResource(_xRadius, _yRadius);
+    }
+
+    private void SpawnResource(float x, float y)
+    {
+        Instantiate(resourcePrefabs, RandomRadiusPosition(x, y), Quaternion.identity);
+    }
+
+    private Vector2 RandomRadiusPosition(float x, float y)
+    {
+        return new Vector2(Random.Range(-x - offset, x + offset), Random.Range(-y - offset, y + offset));
+    }
+}
