03da44d08a5b3f7afd91cae47112bf1a8abd2474
luckynee <janspurba64@gmail.com>
2021-12-15 20:58:46 +0700

    problem2

---
+++ Assets/Scripts/Problem2/BallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    //Components
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        SetVelocity(speed);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void SetVelocity(float velocity)
+    {
+        rb2d.velocity = Random.onUnitSphere * velocity;
+    }
+}
