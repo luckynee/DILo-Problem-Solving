56604a4027c3c495028ab32b6a009e4cf4aeaac8
luckynee <janspurba64@gmail.com>
2021-12-16 05:42:10 +0700

    problem8

---
+++ Assets/Scripts/BallController7.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController7 : MonoBehaviour
+{
+    public UnityEvent OnGetResource;
+    public float speed = 5f;
+
+    private Vector2 inputVector = Vector2.zero;
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            inputVector = GetDirection();
+            MoveCircle(inputVector, speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Resource")
+        {
+            Destroy(collision.gameObject);
+            OnGetResource.Invoke();
+            Debug.Log("Get Resource");
+        }
+    }
+
+    private void MoveCircle(Vector2 input, float velocity)
+    {
+        rb2d.velocity = input * velocity;
+    }
+
+    public Vector2 GetDirection()
+    {
+        Debug.Log("Clicked");
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z));
+        Vector3 direction = worldMousePosition - this.transform.position;
+        direction.Normalize();
+        return (Vector2)direction;
+    }
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public GameObject resourcePrefabs;
+    public int count = 5;
+
+    [Header("Respawn Radius")]
+    public Transform xPoint;
+    public Transform yPoint;
+    public float offset = 0f;
+
+    private float _xRadius;
+    private float _yRadius;
+
+    private void Awake()
+    {
+        _xRadius = xPoint.position.x;
+        _yRadius = yPoint.position.y;
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < count; i++)
+            SpawnResource(_xRadius, _yRadius);
+    }
+
+    public void SpawnResourceDelay()
+    {
+        Invoke("SpawnResource", 3f);
+    }
+
+    private void SpawnResource()
+    {
+        SpawnResource(_xRadius, _yRadius);
+    }
+
+    private void SpawnResource(float x, float y)
+    {
+        Debug.Log("...Trying to respawn");
+        Vector2 pos = RandomRadiusPosition(x, y);
+
+        // If somehow 'pos' inside player circle collider, repeat the function with same parameter
+        if(GameObject.FindGameObjectWithTag("Player").GetComponent<CircleCollider2D>().bounds.Contains(pos))
+        {
+            Debug.Log("...Respawn failed");
+            SpawnResource(x, y);
+            return;
+        }
+
+        Debug.Log("Respawned");
+        Instantiate(resourcePrefabs, pos, Quaternion.identity);
+    }
+
+    private Vector2 RandomRadiusPosition(float x, float y)
+    {
+        return new Vector2(Random.Range(-x - offset, x + offset), Random.Range(-y - offset, y + offset));
+    }
+}
