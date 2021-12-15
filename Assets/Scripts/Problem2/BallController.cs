3ba4eb6bd44170a7e884007ab56a80ec5abdf108
luckynee <janspurba64@gmail.com>
2021-12-16 02:29:20 +0700

    problem4

--- Assets/Scripts/Problem2/BallController.cs
+++ Assets/Scripts/Problem2/BallController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 5f;
+
+    private Vector2 inputVector = Vector2.zero;
 
     Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        MoveCircle(speed);
     }
 
-    private void MoveCircle(float velocity)
+    private void FixedUpdate()
     {
-        rb2d.velocity = Random.onUnitSphere * velocity;
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+        MoveCircle(inputVector, speed);
+    }
+
+    private void MoveCircle(Vector2 input, float velocity)
+    {
+        rb2d.velocity = inputVector * velocity;
     }
 }
